d4c996cc4db1f98ddf08e5e14f49d242b5862029
Andrea Gotelli <gotelliandrea@gmail.com>
2021-01-21 23:26:39 +0100

    added the files

--- robot_simulation_state_machines/src/robot_simulation_state_machines/find_state.py
+++ robot_simulation_state_machines/src/robot_simulation_state_machines/find_state.py
@@ -1,4 +1,32 @@
-# This Python file uses the following encoding: utf-8
+import rospy
+import smach
+import smach_ros
+import actionlib
+from explore_lite.msg import ExploreAction, ExploreGoal
+import robot_simulation_state_machines.image_processing as imp
 
-# if__name__ == "__main__":
-#     pass
+
+class Find(smach.State):
+    ##
+    #   \brief The __init__ constructor initializes the state outcome and input output keys
+    def __init__(self):
+            smach.State.__init__(self, outcomes=['track'])
+            self.explore_client = actionlib.SimpleActionClient('explore_action_server',ExploreAction)
+    ##
+    #   \brief  The member function executing the state behavior
+    #   \param userdata Is the structure containing the data shared among states.
+    #   \return a string consisting of the state outcome
+    #
+    #
+    def execute(self, userdata):
+        global ball_detected
+        self.explore_client.wait_for_server()
+
+        goal = ExploreGoal()
+
+        self.explore_client.send_goal(goal)
+        while not rospy.is_shutdown():
+            if imp.ball_detected:
+                print("A new ball has been detected!")
+                self.explore_client.cancel_all_goals()
+                return 'track'
--- robot_simulation_state_machines/src/robot_simulation_state_machines/interact_state.py
+++ robot_simulation_state_machines/src/robot_simulation_state_machines/interact_state.py
@@ -1,4 +1,73 @@
-# This Python file uses the following encoding: utf-8
+import rospy
+import smach
+import smach_ros
+import random
+from geometry_msgs.msg import Pose
+from robot_simulation_messages.srv import PersonCommand
+from robot_simulation_state_machines.move_state import isTired
+from robot_simulation_state_machines.reach_goal import reachPosition
 
-# if__name__ == "__main__":
-#     pass
+import robot_simulation_state_machines.image_processing as imp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Interact(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes=['find','tired','stop_play'],
+                             input_keys=['interact_fatigue_counter_in'],
+                             output_keys=['interact_fatigue_counter_out','room_to_find'])
+
+        self.person_srv_client = rospy.ServiceProxy('/person_decision', PersonCommand)
+        self.time_in_play = 0.0
+
+    def execute(self, userdata):
+        min_time = 100
+        max_time = 200
+        self.time_in_play = random.randint(min_time, max_time)
+        init_time = rospy.Time.now().to_sec()
+        while not rospy.is_shutdown():
+            time_elapsed = rospy.Time.now().to_sec() - init_time
+            if time_elapsed >= self.time_in_play:
+                return 'stop_play'
+            person_pose = Pose()
+            person_pose.position.x = -5
+            person_pose.position.y = 8
+            print("Reaching person position")
+            reachPosition(person_pose, wait=True)
+
+            rospy.wait_for_service('/person_decision')
+            print("Waiting for a command from the person")
+            desired = self.person_srv_client()
+            print("Command received: go to the", desired.room)
+            available_room = False
+            for room in imp.rooms_list:
+                if room[1].is_registered and room[0] == desired.room :
+                    available_room = True
+                    print("Going to room ", room[0] )
+                    reachPosition(room[1].position,  wait=True)
+                    #   Increase level of fatigue
+                    userdata.interact_fatigue_counter_out = userdata.interact_fatigue_counter_in + 1
+                    #   Print a log to show the level of fatigue
+                    print('Level of fatigue : ', userdata.interact_fatigue_counter_in)
+                    #   Check is the robot is tired
+                    if isTired(userdata.interact_fatigue_counter_in) :
+                        #   Print a log to inform about the fact that the robot is tired
+                        print('Robot is tired of moving...')
+                        #   Return 'tired' to change the state
+                        return 'tired'
+            if not available_room :
+                print("The room", desired.room, "is not available yet...")
+                userdata.room_to_find = desired.room
+                return 'find'
