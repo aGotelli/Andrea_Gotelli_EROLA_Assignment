9c7d2e39f4cfe4cfa931da7c8da8f24c836aebd1
Andrea Gotelli <gotelliandrea@gmail.com>
2021-01-26 15:52:28 +0100

    completed the person behavior
    added the part for randomly publish a message

--- robot_simulation_state_machines/src/person.cpp
+++ robot_simulation_state_machines/src/person.cpp
@@ -1,4 +1,5 @@
 #include "ros/ros.h"
+#include <std_msgs/String.h>
 #include <robot_simulation_messages/PersonCommand.h>
 #include <algorithm>
 #include <ctime>
@@ -14,7 +15,23 @@ static std::vector<std::string> rooms {
 };
 
 
-bool room_selection(robot_simulation_messages::PersonCommand::Request  &req,
+const inline ros::Duration waitingPeriod(const int min=300, const int max=600)
+{
+  return ros::Duration( rand()%(max-min + 1) + min );
+}
+
+
+static ros::Publisher command_to_play;
+void CallToPlay(ros::TimerEvent, ros::Timer* timer)
+{
+  command_to_play.publish( std_msgs::String() );
+  timer->stop();
+  timer->setPeriod( waitingPeriod() );
+  timer->start();
+}
+
+
+bool roomSelection(robot_simulation_messages::PersonCommand::Request  &,
          robot_simulation_messages::PersonCommand::Response &res)
 {
   std::random_shuffle(rooms.begin(), rooms.end());
@@ -24,14 +41,20 @@ bool room_selection(robot_simulation_messages::PersonCommand::Request  &req,
 
 int main(int argc, char **argv)
 {
-  std::srand ( unsigned ( std::time(0) ) );
+  std::srand ( unsigned ( std::time( nullptr) ) );
 
   ros::init(argc, argv, "person");
   ros::NodeHandle nh;
 
-  ros::ServiceServer service = nh.advertiseService("/person_decision", room_selection);
+  ros::ServiceServer service = nh.advertiseService("/person_decision", roomSelection);
+
+  command_to_play = nh.advertise<std_msgs::String>("PersonCommand", 1);
+
+  ros::Timer auto_caller;
+  auto_caller = nh.createTimer( waitingPeriod(), boost::bind(CallToPlay, _1, &auto_caller), true );
 
   ros::spin();
 
   return 0;
+
 }
